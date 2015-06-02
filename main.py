82fca3584555592cf1c40cf5cbf95e10cf7760c8
wklin <wklin@iis.sinica.edu.tw>
2015-06-02 14:50:58 +0800

    convert settings to command

--- main.py
+++ main.py
@@ -4,22 +4,29 @@
 3. Invoke commands with InsertSymbol with args.
 """
 
+import os
+import json
 import sublime, sublime_plugin
 
 
 
 class InsertSymbolCommand(sublime_plugin.TextCommand):
-	def run(self, edit, name = ""):
+	def run(self, edit, text = ""):
 		view = self.view
-		view.insert(edit, view.sel()[0].a, name)
+		view.insert(edit, view.sel()[0].a, text)
 
 
 def get_settings():
 	setting = sublime.load_settings('LatexSymbols.sublime-settings')
 	sym_list = setting.get('symbol_list', [])
-	# write sym_list to Default.sublime-commands
+	cmd_list = []
 	for sym in sym_list:
-		print(sym['command'], sym['text'])
+		cmd = {'caption': sym['caption'], 'command': 'insert_symbol', 'args': {'text': sym['text']}}
+		cmd_list.append(cmd)
+	# write sym_list to LatexSymbols.sublime-commands
+	commandFilePath = os.path.join(sublime.packages_path(), 'User', 'LatexSymbols.sublime-commands')
+	f = open(commandFilePath, 'w')
+	json.dump(cmd_list, f)
 
 def plugin_loaded():
 	get_settings()
